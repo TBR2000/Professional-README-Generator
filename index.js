60ba0ee08f4c12d7f394708962983d9e3a985bb5
Tim <SESA493999@se.com>
2021-05-02 17:55:44 +1000

    questions written, write file code written

--- index.js
+++ index.js
@@ -1,13 +1,71 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      message: 'What is the title of your project?',
+      name: 'Title',
+    },
+    {
+      type: 'input',
+      message: 'Give a brief description of your project',
+      name: 'Descript',
+    },
+    {
+      type: 'input',
+      message: 'provide install instrutions for your project',
+      name: 'install',
+    },
+    {
+      type: 'input',
+      message: 'Provide usage instrutions for your project',
+      name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Provide details on contributing to your project',
+        name: 'contrib',
+      },
+    {
+        type: 'input',
+        message: 'Provide test instrutions for your project',
+        name: 'test',
+      },
+    {
+        type: 'input',
+        message: 'What license is your project using',
+        name: 'license',
+      },
+    {
+        type: 'input',
+        message: 'What is your github?',
+        name: 'github',
+      },
+    {
+        type: 'input',
+        message: 'What email should questions be directed to?',
+        name: 'email',
+      },
+  ])
+
+  .then((response) =>
+  fs.writeFile('README.md'), (err) =>
+  err ? console.error(err) : console.log('README created!')
+  
+);
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+//const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+//function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+//function init() {}
 
 // Function call to initialize app
-init();
+//init()
