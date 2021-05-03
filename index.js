ad7689421a51a8679e9dbc0aa52420979820ddf8
Tim <SESA493999@se.com>
2021-05-03 17:57:52 +1000

    Index complete, generateMarkdown functions being worked on

--- index.js
+++ index.js
@@ -1,144 +1,85 @@
 // TODO: Include packages needed for this application
 const fs = require('fs');
 const inquirer = require('inquirer');
-let licensing = require('./utils/generateMarkdown')
-
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      message: 'What is the title of your project?',
-      name: 'Title',
-    },
-    {
-      type: 'input',
-      message: 'Give a brief description of your project',
-      name: 'Descript',
-    },
-    {
-      type: 'input',
-      message: 'provide install instrutions for your project',
-      name: 'install',
-    },
-    {
-      type: 'input',
-      message: 'Provide usage instrutions for your project',
-      name: 'usage',
-    },
-    {
-        type: 'input',
-        message: 'Provide details on contributing to your project',
-        name: 'contrib',
-      },
-    {
-        type: 'input',
-        message: 'Provide test instrutions for your project',
-        name: 'test',
-      },
-    {
-        type: 'list',
-        message: 'What license is your project using',
-        name: 'licenseResponse',
-        choices: ['MIT', 'Apache', 'Apache 2', 'MIT/Apache-2.0', 'lgpl_2_1', 'GPL', 'GPL(>=2)', 'BSD'],
-      },
-    {
-        type: 'input',
-        message: 'What is your github?',
-        name: 'github',
-      },
-    {
-        type: 'input',
-        message: 'What is the name of your repository?',
-        name: 'repo',
-      },
-    {
-        type: 'input',
-        message: 'What email should questions be directed to?',
-        name: 'email',
-      },
-
-    {
-        type: 'input',
-        message: 'Provide links to screenshots of your project',
-        name: 'images',
-      },
-
-    {
-        type: 'input',
-        message: 'What are the links to your deployed project and Repo',
-        name: 'links',
-      }, 
-  ])
-
-.then((response) => {
-  renderLicenseBadge(licenseResponse);
-  renderLicenseLink(licenseResponse);
-  renderLicenseSection(licenseResponse);
-
-})
-    //render license and badge here
-  
-  .then((response) =>
-  fs.writeFile('README.md',`
-# ${title}
-
-${badge}
-
-## Description
-${descript}
-
-## Table of Contents
-
-- [Installation Instrutions](#InstallationInstrutions)
-- [Usage information](#usageinformation)
-- [Contribution information](#contributioninformation)
-- [Testing](#testing)
-- [License](#license)
-- [Questions](#questions)
-- [Screenshots](#screenshots)
-- [Links](#links)
-
-## Installation Instrutions
-${install}
-
-## Usage information
-${usage}
-
-## Contribution information
-${contrib}
-
-## Testing
-${test}
-
-## License
-${license}
-
-## Questions
-This project is located on my github: ${github}, In the repository ${repo}
-
-If you have any questions please contact me at: ${email}
-
-##Screenshots
-Screenshots of the deployed project:
-${images}
-
-##Links 
-Links to the deployed project and Github Respository:
-${links}
-`), (err) =>
-  err ? console.error(err) : console.log('README created!')
-  
-);
-
+const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
-//const questions = [];
+const questions = [
+  {
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'Title',
+  },
+  {
+    type: 'input',
+    message: 'Give a brief description of your project',
+    name: 'Descript',
+  },
+  {
+    type: 'input',
+    message: 'provide install instrutions for your project',
+    name: 'install',
+  },
+  {
+    type: 'input',
+    message: 'Provide usage instrutions for your project',
+    name: 'usage',
+  },
+  {
+      type: 'input',
+      message: 'Provide details on contributing to your project',
+      name: 'contrib',
+    },
+  {
+      type: 'input',
+      message: 'Provide test instrutions for your project',
+      name: 'test',
+    },
+  {
+      type: 'list',
+      message: 'What license is your project using',
+      name: 'licenseResponse',
+      choices: ['MIT', 'Apache', 'Apache 2', 'MIT/Apache-2.0', 'lgpl_2_1', 'GPL', 'GPL(>=2)', 'BSD'],
+    },
+  {
+      type: 'input',
+      message: 'What is your github?',
+      name: 'github',
+    },
+  {
+      type: 'input',
+      message: 'What is the name of your repository?',
+      name: 'repo',
+    },
+  {
+      type: 'input',
+      message: 'What email should questions be directed to?',
+      name: 'email',
+    },
+
+  {
+      type: 'input',
+      message: 'Provide links to screenshots of your project',
+      name: 'images',
+    },
+
+  {
+      type: 'input',
+      message: 'What are the links to your deployed project and Repo',
+      name: 'links',
+    }, 
+];
 
 // TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+      fs.writeFile(fileName, data, (err) => err ? console.error(err) : console.log('README created!'))
+}
 
 // TODO: Create a function to initialize app
-//function init() {}
-
+function init() {
+  inquirer.prompt(questions).then((data) => {
+            writeToFile('README.md', generateMarkdown(data));
+  });
+}
 // Function call to initialize app
-//init()
+init()
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,87 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(licenseResponse) {
-  if(!licenseResponse){
-    let badge = {}
+
+function renderLicenseBadge() {
+  if(!data.licenseResponse){
+    let bagde= {}
   }else{
-    let badge = 'https://img.shields.io/github/license/' + github + '/' + repo + '?style=plastic'
+    let badge = 'https://img.shields.io/github/license/' + data.github + '/' + data.repo + '?style=plastic'
   }
   return (badge)
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(licenseResponse) {}
+function renderLicenseLink() {
+  if(!data.licenseResponse){
+    let licenseUrl = {}
+  }else{
+    let licenseUrl = 'https://opensource.org/licenses/' + data.licenseResponse
+  }
+ return(licenseUrl)
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(licenseResponse) {}
+
+function renderLicenseSection(licenseUrl){
+  if (!licenseUrl){
+    let license = {}
+  }else{
+    let license = licenseUrl
+  }
+  return(license)
+
+};
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+# ${data.title}
 
-`;
-}
+${badge}
+
+## Description
+${data.descript}
+
+## Table of Contents
+
+- [Installation Instrutions](#InstallationInstrutions)
+- [Usage information](#usageinformation)
+- [Contribution information](#contributioninformation)
+- [Testing](#testing)
+- [License](#license)
+- [Questions](#questions)
+- [Screenshots](#screenshots)
+- [Links](#links)
+
+## Installation Instrutions
+${data.install}
+
+## Usage information
+${data.usage}
+
+## Contribution information
+${data.contrib}
+
+## Testing
+${data.test}
+
+## License
+${license}
+
+## Questions
+This project is located on my github: ${data.github}, In the repository ${data.repo}
+
+If you have any questions please contact me at: ${data.email}
+
+##Screenshots
+Screenshots of the deployed project:
+${data.images}
+
+##Links 
+Links to the deployed project and Github Respository:
+${data.links}
+`;}
 
 module.exports = generateMarkdown;
